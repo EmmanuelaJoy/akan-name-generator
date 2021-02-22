5dc8757527cb8f660c986576586ca85e5b4b387c
Emmanuela Joy <emmajoy81@gmail.com>
2021-02-22 08:13:32 +0300

    add control flows

--- js/script.js
+++ js/script.js
@@ -9,7 +9,35 @@ window.addEventListener("DOMContentLoaded", function () {
         event.preventDefault();
         console.log("Form submitted")
 
-        let birthdateInput = document.querySelector("#date");
+        let birthdateInput = document.querySelector("#date").value;
+        const gender = document.querySelector("#gender").value;
 
-    })
-})
+        let dayBorn = newDate(birthdateInput);
+        dayBorn.getDay() = dob
+
+        let akanNameRow = document.createElement("tr");
+        akanNamesData = document.createElement("td");
+        akanTbody.appendChild(nameRow);
+
+        let akanMaleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+        let akanFemaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+        if (gender == "Male") {
+            nameList.push(akanMaleName[dob]);
+            showAkanName();
+        } else if (gender == "Female") {
+            nameList.push(akanFemaleName[dob]);
+            showAkanName();
+        } else {
+            alert("Kindly select your gender first");
+        }
+
+        function showAkanName() {
+            nameList.forEach(function (name) {
+                namesData.textContent = name;
+                nameRow.appendChild(akanNamesData);
+                nameTbody.appendChild(akanNameRow);
+            });
+        }
+    });
+});
