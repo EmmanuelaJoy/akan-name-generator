c00c61933088886b84f718882b9ce1e109d10e72
Emmanuela Joy <emmajoy81@gmail.com>
2021-02-22 06:28:15 +0300

    add a jumbotron

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
-window.addEventListener("DOMContentLoaded", function() {
-    console.log("Content Loaded");}
+var day = document.querySelector("form select[name=day]");
+var month = document.querySelector("form select[name=month]");
+var year = document.querySelector("form input[name=year]");
+var gender = document.getElementById("gender").value;
+
+month.addEventListener("input", updateDay);
+year.addEventListener("input", updateDay);
